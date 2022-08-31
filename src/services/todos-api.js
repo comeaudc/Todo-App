192a6c548dce028ae2490acf2e2d9ce4e6a51e32
Dylan Comeau <comeaudc@comcast.net>
2022-08-31 15:24:05 -0400

    Updated URL destination for heroku

--- src/services/todos-api.js
+++ src/services/todos-api.js
@@ -2,32 +2,32 @@
 import axios from 'axios'
 
 export const getTodos = () => {
-    const URL = 'https://todoapidcc.herokuapp.com/'
+    const URL = 'https://todoapidcc.herokuapp.com/todos'
     const response = axios.get(URL)
     console.log(response)
     return response
 }
 
 export const getTodo = (id) => {
-    const URL = `https://todoapidcc.herokuapp.com/${id}`
+    const URL = `https://todoapidcc.herokuapp.com/todos/${id}`
     const response = axios.get(URL)
     return response      
 }
 
 export const deleteTodo = (id) => {
-    const URL = `https://todoapidcc.herokuapp.com/${id}`
+    const URL = `https://todoapidcc.herokuapp.com/todos/${id}`
     const response = axios.delete(URL)
     return response
 }
 
 export const editTodo = (id, updatedTodo) => {
-    const URL = `https://todoapidcc.herokuapp.com/${id}`
+    const URL = `https://todoapidcc.herokuapp.com/todos/${id}`
     const response = axios.put(URL, updatedTodo)
     return response
 }
 
 export const createTodo = (todo) => {
-    const URL = 'https://todoapidcc.herokuapp.com/'
+    const URL = 'https://todoapidcc.herokuapp.com/todos'
     const response = axios.post(URL, todo)
     return response
 }
